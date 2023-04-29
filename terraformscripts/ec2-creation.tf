82a440ff50893f5f6a7d49eb4c3688ec499b3f12
petergillgithub <121865982+petergillgithub@users.noreply.github.com>
2023-04-29 13:32:23 +0100

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,15 +1,15 @@
 provider "aws"{
-region = "ap-southeast-1"
+   region = "eu-west-2"
 }
 
-resource "aws_instance" "AWSInstance" {
-     ami = "ami-0319ac76374b9fe74"
-	 instance_type = "t2.micro"
-	 key_name = "devopsmss"
-	 security_groups = ["launch-wizard-2"]
-	 tags = {
-	 Name = "tomcatservers"
-	 }
+resource "aws_instance" "AWSInstatance"{
+	ami = "ami-08d9bb4bfe39be5c2"
+	instance_type = "t2.micro"
+	key_name = "ansible"
+	security_groups = ["launch-wizard-2"]
+	tags = {
+	Name = "tomcatservers"
+	}
 }
 
 
